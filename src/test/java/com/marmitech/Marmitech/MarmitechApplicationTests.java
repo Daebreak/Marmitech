9ad8df0abc4a8613b83bcb6e877d905b7eb2c5b0
daebreak <daebreak@FaculdadeUni.myguest.virtualbox.org>
2025-08-10 19:36:31 -0300

    Base

---
+++ src/test/java/com/marmitech/Marmitech/MarmitechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marmitech.Marmitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarmitechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
