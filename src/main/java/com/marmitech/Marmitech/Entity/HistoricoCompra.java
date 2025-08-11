22cfa9b3dd46344640eb2bd5c6e7ea05d73133f9
Daebreak <daebreak-@hotmail.com>
2025-08-10 22:52:31 -0300

    Finalizado historico compra e pedido item services, add validacao

--- src/main/java/com/marmitech/Marmitech/Entity/HistoricoCompra.java
+++ src/main/java/com/marmitech/Marmitech/Entity/HistoricoCompra.java
@@ -25,4 +25,28 @@ public class HistoricoCompra {
     private String data_evento;
     private String tipo_evento;
     private String descricao;
+
+    public HistoricoCompra(int pedidoId, int clienteId, String data_evento, String tipo_evento, String descricao) {
+        if(pedidoId <= 0){
+            throw new IllegalArgumentException("pedidoId não pode ser menor que 1.");
+        }
+        if(clienteId <= 0){
+            throw new IllegalArgumentException("clienteId não pode ser menor que 1.");
+        }
+        if(data_evento == null || data_evento.isBlank()){
+            throw new IllegalArgumentException("data_evento não pode ser null ou estar em branco.");
+        }
+        if(tipo_evento == null || tipo_evento.isBlank()){
+            throw new IllegalArgumentException("tipo_evento não pode ser null ou estar em branco.");
+        }
+        if(descricao == null || descricao.isBlank()){
+            throw new IllegalArgumentException("pedidoId não pode ser null ou estar em branco.");
+        }
+
+        this.pedidoId = pedidoId;
+        this.clienteId = clienteId;
+        this.data_evento = data_evento;
+        this.tipo_evento = tipo_evento;
+        this.descricao = descricao;
+    }
 }
--- src/main/java/com/marmitech/Marmitech/Entity/PedidoItem.java
+++ src/main/java/com/marmitech/Marmitech/Entity/PedidoItem.java
@@ -24,4 +24,31 @@ public class PedidoItem {
     private int quantidade;
     private int precoUnitarioPedido;
     private int subtotal;
+
+    public PedidoItem(int pedidoId, int produtoId, int quantidade, int precoUnitarioPedido, int subtotal) {
+
+        if (pedidoId <= 0) {
+            throw new IllegalArgumentException("pedidoId não pode ser menor que 1.");
+        }
+        if (produtoId <= 0) {
+            throw new IllegalArgumentException("produtoId não pode ser menor que 1");
+        }
+        if (quantidade <= 0) {
+            throw new IllegalArgumentException("quantidade não pode ser menor que 1.");
+        }
+        if (precoUnitarioPedido < 0) {
+            throw new IllegalArgumentException("precoUnitarioPedido não pode ser menor que 1.");
+        }
+        if (subtotal < 0) {
+            throw new IllegalArgumentException("subtotal não pode ser menor que 1.");
+        }
+
+        this.pedidoId = pedidoId;
+        this.produtoId = produtoId;
+        this.quantidade = quantidade;
+        this.precoUnitarioPedido = precoUnitarioPedido;
+        this.subtotal = subtotal;
+    }
+
+    
 }
