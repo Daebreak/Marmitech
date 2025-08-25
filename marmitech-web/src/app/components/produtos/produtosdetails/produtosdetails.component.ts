b8bbc7d5804b9cafef2f0b2fe555326c2990ff57
Daebreak <daebreak-@hotmail.com>
2025-08-24 23:39:40 -0300

    Produto cadastro e edição agora no formato modal

--- marmitech-web/src/app/components/produtos/produtosdetails/produtosdetails.component.ts
+++ marmitech-web/src/app/components/produtos/produtosdetails/produtosdetails.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
 import { Produto } from '../../../models/produto';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-produtosdetails',
@@ -11,7 +12,18 @@ import { Produto } from '../../../models/produto';
   styleUrl: './produtosdetails.component.scss'
 })
 export class ProdutosdetailsComponent {
-  produto: Produto = new Produto(
+  router = inject(ActivatedRoute);
+  routerSaver = inject(Router);
+  
+  constructor() {
+    const id = this.router.snapshot.paramMap.get('id');
+    console.log(id);
+    if (id) {
+      this.findById(parseInt(id));
+    }
+  }
+  
+  @Input("produto") produto: Produto = new Produto(
     { id: 0, 
       nome: '', 
       descricao: '', 
@@ -21,7 +33,39 @@ export class ProdutosdetailsComponent {
       estoque: 0, 
       sku: '' });
 
-  salvar() {
-    alert('Produto salvo com sucesso!');
+  @Output("retorno") retorno = new EventEmitter<Produto>();
+      
+    findById(id: number) {
+      let produtoRetornado: Produto = new Produto({
+        id: id,
+        nome: 'Produto ' + id,
+        descricao: 'Descrição do Produto ' + id,
+        categoria: 'Categoria A',
+        dataCadastro: '2023-10-01',
+        precoUnitario: 100.00 * id,
+        estoque: 50 - id,
+        sku: 'SKU00' + id
+      });
+      this.produto = produtoRetornado;
+    }
+
+    salvar() {
+    if(this.produto.id) {
+      Swal.fire({
+        title: 'Atualizado!',
+        icon: 'success',
+        confirmButtonText: 'Sim',
+      })
+      this.routerSaver.navigate(['admin/produtos'], {state: { produtoEditado: this.produto }});
+    } else {
+      Swal.fire({
+        title: 'Cadastrado!',
+        icon: 'success',
+        confirmButtonText: 'Sim',
+      })
+      this.routerSaver.navigate(['admin/produtos'], {state: { produtoNovo: this.produto }});
+    }
+
+    this.retorno.emit(this.produto);
   }
 }
--- marmitech-web/src/app/components/produtos/produtoslist/produtoslist.component.ts
+++ marmitech-web/src/app/components/produtos/produtoslist/produtoslist.component.ts
@@ -1,18 +1,34 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, TemplateRef, ViewChild } from '@angular/core';
 import { Produto } from '../../../models/produto';
 import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
+import {MdbModalModule, MdbModalRef, MdbModalService} from 'mdb-angular-ui-kit/modal';
+import { ProdutosdetailsComponent } from "../produtosdetails/produtosdetails.component";
 
 @Component({
   selector: 'app-produtoslist',
-  imports: [RouterLink],
+  imports: [RouterLink, MdbModalModule, ProdutosdetailsComponent],
   templateUrl: './produtoslist.component.html',
   styleUrl: './produtoslist.component.scss'
 })
 export class ProdutoslistComponent {
   lista: Produto[] = [];
+  produtoEdit: Produto = new Produto(
+    { id: 0, 
+      nome: '', 
+      descricao: '', 
+      categoria: '',
+      dataCadastro: '', 
+      precoUnitario: 0, 
+      estoque: 0, 
+      sku: '' });
+
+  modalService = inject(MdbModalService);
+  @ViewChild('modalProdutoDetalhe') modalProdutoDetalhe!: TemplateRef<any>;
+  modalRef!: MdbModalRef<any>;
 
   constructor() {
-    this.lista.push( new Produto({
+    this.lista.push(new Produto({
       id: 1,
       nome: 'Produto 1',
       descricao: 'Descrição do Produto 1',
@@ -21,9 +37,9 @@ export class ProdutoslistComponent {
       precoUnitario: 100.00,
       estoque: 50,
       sku: 'SKU001'
-    }) );
+    }));
 
-    this.lista.push( new Produto({
+    this.lista.push(new Produto({
       id: 2,
       nome: 'Produto 2',
       descricao: 'Descrição do Produto 2',
@@ -32,9 +48,9 @@ export class ProdutoslistComponent {
       precoUnitario: 150.00,
       estoque: 30,
       sku: 'SKU002'
-    }) );
+    }));
 
-    this.lista.push( new Produto({
+    this.lista.push(new Produto({
       id: 3,
       nome: 'Produto 3',
       descricao: 'Descrição do Produto 3',
@@ -43,10 +59,61 @@ export class ProdutoslistComponent {
       precoUnitario: 200.00,
       estoque: 20,
       sku: 'SKU003'
-    }) );
+    }));
+
+    history.state.produtoNovo && this.lista.push(history.state.produtoNovo);
+    history.state.produtoEditado && (this.lista = this.lista.map(p => p.id === history.state.produtoEditado.id ? history.state.produtoEditado : p));
+
   }
 
-  deletar(){
-    //
+  editById(produto: Produto) {
+    this.produtoEdit = Object.assign({}, produto);
+    this.modalRef = this.modalService.open(this.modalProdutoDetalhe);
+  }
+
+  new() {
+    this.produtoEdit = new Produto(
+      { id: 0, 
+        nome: '', 
+        descricao: '', 
+        categoria: '',
+        dataCadastro: '', 
+        precoUnitario: 0, 
+        estoque: 0, 
+        sku: '' });
+    this.modalRef = this.modalService.open(this.modalProdutoDetalhe);
+  }
+
+
+  retornoDetalhe(produto: Produto) {
+    if(produto.id) {
+      this.lista = this.lista.map(p => p.id === produto.id ? produto : p);
+    } else {
+      produto.id = this.lista.length + 1;
+      this.lista.push(produto);
+    }
+    this.modalRef.close();    
+  }
+
+
+  deleteById(produto: Produto) {
+    Swal.fire({
+      title: 'Confirma a exclusão do produto ' + produto.nome + '?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sim',
+      cancelButtonText: 'Não'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.lista = this.lista.filter(p => p.id !== produto.id);
+        Swal.fire(
+          'Excluído!',
+          'Produto ' + produto.nome + ' excluído com sucesso.',
+          'success'
+        );
+      }
+    });
   }
 }
