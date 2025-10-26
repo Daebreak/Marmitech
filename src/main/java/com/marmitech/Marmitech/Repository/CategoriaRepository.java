c0a4ac89d49c56247829cbce5f21cc7d4e29c239
josea95 <joseademir.foz@gmail.com>
2025-10-26 02:37:02 -0300

    CategoriaServiceTest adicionado

--- src/main/java/com/marmitech/Marmitech/Repository/CategoriaRepository.java
+++ src/main/java/com/marmitech/Marmitech/Repository/CategoriaRepository.java
@@ -1,8 +1,7 @@
 package com.marmitech.Marmitech.Repository;
 import com.marmitech.Marmitech.Entity.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+
 public interface CategoriaRepository extends JpaRepository <Categoria,Integer> {
-    List<Categoria>findById(int id);
 
 }
